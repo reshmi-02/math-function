a9d6279cef9e6cf158ba1972883100eeeca414f7
reshmi-02 <reshmihash123@gmail.com>
2023-07-26 11:06:28 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("math floor");
+console.log(Math.floor(5.3));
+
+console.log("math ceil");
+console.log(Math.ceil(5.01));
+
+console.log("math round");
+console.log(Math.round(5.6));
+
+console.log("math rondom");
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*100));
+
+console.log("math abs");
+console.log(Math.abs(-2));
+
+console.log("math sign");
+console.log(Math.sign(10));
+console.log(Math.sign(-10));
+
+console.log("math sqrt");
+console.log(Math.sqrt(2));
+
+console.log("math cbrt");
+console.log(Math.cbrt(9));
+
+console.log("math min");
+console.log(Math.min(1,4,5));
+
+console.log("math max");
+console.log(Math.max(4,6,7));
